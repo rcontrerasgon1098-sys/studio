fd1fa29e825b12b559c477fdf6fe77aad2429f24
Ramiro Contreras <contrerasramiro1098@gmail.com>
2026-02-10 12:19:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1315843809-edcbd",
+  "appId": "1:672837877614:web:334fa8455ec0d19aa3cb7f",
+  "apiKey": "AIzaSyB-ixOLOguW2peEJ8kFuyokf5XwpRJcdsw",
+  "authDomain": "studio-1315843809-edcbd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "672837877614"
+};
